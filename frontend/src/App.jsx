447adbb61a3c01765b4425522b3cb9a17c5bd7f2
Divya Pahuja <divyapahuja31@gmail.com>
2025-06-24 13:53:15 +0530

    creating Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home'
-import MyEvents from "./pages/MyEvents";
-import Events from './pages/Events'
-
-import Footer from './components/Footer.jsx'
+import Home from './pages/Home';
+import MyEvents from './pages/MyEvents';
+import Events from './pages/Events';
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
 import AuthPage from './pages/AuthPage';
 import ProfileCard from './pages/ProfileCard';
 import CreateEventForm from './pages/CreateEventForm';
+
 const App = () => {
+  const userType = 'student'; 
+
   return (
-    <>
     <Router>
-      <div>
-        <div>
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" replace />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/myevents" element={<MyEvents />} />
-            <Route path="/events" element={<Events />} />
-            <Route path='/profilecard' element={<ProfileCard/>} />
-            <Route path="/login" element={<AuthPage />} />
-            <Route path="/createevent" element={<CreateEventForm />} />
-          </Routes>
-        </div>
-      </div>
-      <Footer/>
+      <Navbar userType={userType} />
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/myevents" element={<MyEvents />} />  
+        <Route path="/profilecard" element={<ProfileCard />} />
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="/create" element={<CreateEventForm />} />
+      </Routes>
+
+      <Footer />
     </Router>
-    </>
   );
 };
 
 export default App;
-
-
-
