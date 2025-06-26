2d5a50881a833f62c2c81961f3336b8e24799c2c
Divya Pahuja <divyapahuja31@gmail.com>
2025-06-26 11:44:03 +0530

    creating navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,9 @@ import Events from './pages/Events';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
 import AuthPage from './pages/AuthPage';
-import ProfileCard from './pages/ProfileCard';
+import ProfileCard from './pages/ProfileCard'
 import CreateEventForm from './pages/CreateEventForm';
+import ClubDirectory from './pages/ClubDirectory';
 
 const App = () => {
   const userType = 'student'; 
@@ -27,6 +28,21 @@ const App = () => {
       </Routes>
 
       <Footer />
+      <div>
+        <div>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" replace />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/myevents" element={<MyEvents />} />
+            <Route path="/events" element={<Events />} />
+            <Route path='/profilecard' element={<ProfileCard/>} />
+            <Route path="/login" element={<AuthPage />} />
+            <Route path="/createevent" element={<CreateEventForm />} />
+            <Route path='/clubpage' element={<ClubDirectory/>}/>
+          </Routes>
+        </div>
+      </div>
+      <Footer/>
     </Router>
   );
 };
